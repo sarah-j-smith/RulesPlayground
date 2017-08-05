39a5a2701200188140ff30615f8814a6d07905af
Sarah Smith <sarah@smithsoft.com.au>
2017-08-05 19:39:36 +1000

    Salience doesn't help retract rules

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -14,25 +14,14 @@ class Player: NSObject
 // The state dictionary is effectively [ Any ] and Strings are OK
 rules.state.setValue("blue", forKey: "color")
 rules.state.setValue(Player(), forKey: "player")
+rules.state.setValue(5.0, forKey: "height")
 
-// This clears the facts so you can do another evaluate - state remains.
-rules.reset()
-
-// Make constant facts constant strings - they have to be NSString
+// Make constant facts constant strings - they have to be NSString 
+// to use in a GKRule since they are NSObjectProtocol objects
 let bigFact = "big" as NSString
-
-// Strings make good facts, and you can set weights as long as they're Float
-let factsAndWeights = [ "red": Float(0.8), bigFact: Float(0.2) ]
-
-
-// You can programmatically assert facts, with starting weights
-for fact in factsAndWeights {
-    
-    // Facts have to be coerced to NSString (NSObject subclass)
-    let factName = fact.key as NSString
-    rules.assertFact(factName, grade: fact.value)
-}
-
+let checkJacketFact = "chequered" as NSString
+let badTaste = "bad taste" as NSString
+let redFact = "red" as NSString
 let grossWeight = 4.0
 
 // The $ symbol means get this key path relative to the state dictionary
@@ -55,19 +44,53 @@ rules.add(GKRule(predicate: pred2, assertingFact: "bulky" as NSString, grade: 1.
 let pred3 = NSPredicate(format: "ALL %@ in facts", ["big", "red"])
 rules.add(GKRule(predicate: pred3, assertingFact: "swol" as NSString, grade: 0.8))
 
+// People over 3ft high are "tall" - add 0.4 to the grade of the tall fact
+let pred4 = NSPredicate(format: "$height > %f", 3.0)
+let heightRule = GKRule(predicate: pred4, assertingFact: "tall" as NSString, grade: 0.4)
+rules.add(heightRule)
+
+// People over 4ft high are even more "tall" - add another 0.4 to the grade of tall
+let pred5 = NSPredicate(format: "$height > %f", 4.0)
+let ballerRule = GKRule(predicate: pred4, assertingFact: "tall" as NSString, grade: 0.4)
+rules.add(ballerRule)
+
+let pred6 = NSPredicate(format: "%@ IN facts", checkJacketFact)
+let takeOffChequeredJacket = GKRule(predicate: pred6, retractingFact: checkJacketFact, grade: 1.1)
+takeOffChequeredJacket.salience = 10000
+// Execute taking off the jacket right at the beginning to avoid dontLikeChecks
+// judgements from our style police rules engine!
+// NOTE - have to set the salience BEFORE adding the rule to the system
+rules.add(takeOffChequeredJacket)
+
+let predNext = NSPredicate(format: "%@ IN facts", checkJacketFact)
+let dontLikeChecks = GKRule(predicate: predNext, assertingFact:badTaste, grade: 0.8)
+rules.add(dontLikeChecks)
+
+rules.reset()
+
+// Strings make good facts, and you can set weights as long as they're Float
+let factsAndWeights = [ redFact: Float(0.5),
+                        bigFact: Float(0.2),
+                        checkJacketFact: Float(1.0) ]
+
+
+// You can programmatically assert facts, with starting weights
+for fact in factsAndWeights {
+    
+    // Facts have to be coerced to NSString (NSObject subclass)
+    let factName = fact.key as NSString
+    rules.assertFact(factName, grade: fact.value)
+}
+
+
 // Processes the rules in order of 'salience' or failing that addition.
 //    First pred is processed which tests that player mass (5) is greater than 4
 //    and thus adds the fact
 rules.evaluate()
 
-// Prints "[red, really big, big, huge]\n"
-print(rules.facts)
 
-// Prints
-//    red = 0.8
-//    really big = 1.0
-//    big = 0.2
-//    huge = 1.0
+// Prints "[red, tall, bulky, swol, bad taste, big]"
+print(rules.facts)
 
 for f in rules.facts
 {
